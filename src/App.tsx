6db608291ccc45eba02b1cccf12ac059e4d85004
kingsley <kingsleymaduebonam@gmail.com>
2024-11-02 12:58:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './Car';  // Import the Car file
+
+const App: React.FC = () => {
+    return (
+        <div className="App min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-2xl font-bold">Vehicle Application</h1>
+        </div>
+    );
+};
+
+export default App;
+
